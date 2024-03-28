e48f5830e49fb662ed14859737fa4397055c70d5
PhilipH <philip.houden.04@gmail.com>
2024-03-28 13:04:04 +0700

    setup environment

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Use compression middleware
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, './public/CSS'), {maxAge: 31536000 }));
+
+// set the view engine to ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Landing Page
+app.get('/', (req, res) => {
+    res.render('landing-page');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
